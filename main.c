36caaa666779cdaa335c2253a31bb7a77e557175
Ganesh Karamsetty <ganesh31pk@gmail.com>
2024-09-15 11:30:39 +0530

    changed led colour to red

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ void GPIOF_SETUP( void )
 void GPIOF_ISR( void )
 {
     // PORT F = ...|SW1|G|B|R|SW2|
-    GPIO_PORTF_DATA_R ^= 0x04 ;
+    GPIO_PORTF_DATA_R ^= 0x02 ;
     GPIO_PORTF_ICR_R = 0x11 ;
     int counter = 10000 ;
     while(counter > 0){
